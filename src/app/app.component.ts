5c585f24b069addb4d6840177f573916315a2ac5
Roel de Man <rjmdeman@gmail.com>
2024-06-22 23:31:08 +0200

    add primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'lib-2.0';
+  
+  private primengConfig = inject(PrimeNGConfig);
+  
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
